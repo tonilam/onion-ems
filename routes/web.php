f5f9aa0e3f1c662308ee0ea3fa6531d03d52537a
Toni Lam <toni_lam@hotmail.com>
2020-12-10 10:17:47 +1000

    Install Sanctum

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['middleware' => ['auth']], function() {
+	Route::post('/tokens/create', function (Request $request) {
+		$token = $request->user()->createToken($request->token_name);
+
+		return ['token' => $token->plainTextToken];
+	});
+});
