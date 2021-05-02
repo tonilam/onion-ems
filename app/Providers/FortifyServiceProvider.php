f88b5aaf2a780c58dbe4141eb0f3a64dee632147
Toni Lam <toni_lam@hotmail.com>
2021-05-03 08:01:31 +1000

    Upgrade Inertial-laravel package

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -28,6 +28,10 @@ class FortifyServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Fortify::loginView(function () {
+            return view('auth.login');
+        });
+
         Fortify::createUsersUsing(CreateNewUser::class);
         Fortify::updateUserProfileInformationUsing(UpdateUserProfileInformation::class);
         Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
